9730009c10a80c6b016bff4e4857965cae366c28
Michael K Schumacher <mschumacher@centos-vm.localdomain>
2017-06-18 10:17:34 -0500

    begin food item program logic and index page

--- app/controllers/food_items_controller.rb
+++ app/controllers/food_items_controller.rb
@@ -1,2 +1,53 @@
 class FoodItemsController < ApplicationController
+  def index
+    @food_items = FoodItem.all
+  end
+  
+  def show
+    @food_item = FoodItem.find(params[:id])
+  end
+  
+  def new
+    @food_item = FoodItem.new
+  end
+  
+  def create
+    @food_item = FoodItem.new(food_item_params)
+    
+    if @food_item.save
+      redirect_to @food_item
+     else
+       render 'new'
+    end
+  end
+  
+  def edit
+    @food_item = FoodItem.find(params[:id])
+  end
+  
+  def update
+    @food_item = FoodItem.find(params[:id])
+    
+    if @food_item.update(food_item_params)
+      redirect_to @food_item
+     else
+       render 'edit'
+    end
+  end
+  
+  
+  private
+    def food_item_params
+      params.require(:food_item).permit(:id,
+                                        :restaurant,
+                                        :name,
+                                        :orderNo,
+                                        :unitPrice,
+                                        :unitType,
+                                        :unitSize,
+                                        :pricePerUnit,
+                                        :locations,
+                                        :created_at,
+                                        :updated_at)
+    end
 end
